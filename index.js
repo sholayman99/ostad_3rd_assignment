050484e31c3e084cfb9037b246428b8168761115
Md Sholayman <mdsholayman998@gmail.com>
2023-08-12 02:09:52 +0600

    all topic solved

--- index.js
+++ index.js
@@ -111,3 +111,78 @@ console.log(MathUtility.multiply(2, 8)); // Output: 16
 console.log(MathUtility.multiply(10, 8)); // Output: 80
 
 
+//Topic-7: javascript es6 class inheritance
+
+class Animal {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    makeSound() {
+        console.log(`${this.name} makes a sound: ${this.sound}`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, sound) {
+        super(name, sound);
+    }
+
+    fetch() {
+        console.log(`${this.name} is fetching the ball!`);
+    }
+}
+
+
+const animal = new Animal("Lion", "Roar");
+
+
+animal.makeSound(); // Output: "Roar"
+
+
+ 
+
+
+const dog = new Dog("Buddy", "Woof");
+
+
+dog.makeSound();
+
+
+dog.fetch(); 
+
+
+//Topic-8: javascript es6 super keyword
+
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, I'm ${this.age} years old, and I'm majoring in ${this.major}.`);
+    }
+}
+
+// Creating instances and using methods
+const person1 = new Person("John", 30);
+person1.introduce();  
+
+const student1 = new Student("Alice", 20, "Computer Science");
+student1.introduce(); 
+
+
