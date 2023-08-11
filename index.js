a572f98b3ddf064a595bd47e628330bda5daf820
Md Sholayman <mdsholayman998@gmail.com>
2023-08-11 21:55:25 +0600

    topic 4 has solved

--- index.js
+++ index.js
@@ -7,12 +7,15 @@
 //Topic-1:javascript For in loop
 
 function printObjectProperties(obj){
+
 if(Object.keys(obj).length===0){
     console.log('Object is empty');
     return
 }
+
   for(property in obj){
     console.log(`${property}:${obj[property]}`);
+
 }
 
 }
@@ -58,5 +61,26 @@ const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
 console.log(mergeArrays(arr1,arr2));
+
+
+
+//topic-4: javascript arrow function
+
+const calculateSquare = num =>{
+    const result = num * num;
+    return result ;
+}
+
+
+console.log(calculateSquare(1)); // Output: 25
+console.log(calculateSquare(2)); // Output: 64
+console.log(calculateSquare(3)); // Output: 64
+console.log(calculateSquare(4)); // Output: 64
+console.log(calculateSquare(5)); // Output: 64
+console.log(calculateSquare(6)); // Output: 64
+console.log(calculateSquare(7)); // Output: 64
+console.log(calculateSquare(8)); // Output: 64
+console.log(calculateSquare(9)); // Output: 64
+console.log(calculateSquare(10)); // Output: 64
 
 
